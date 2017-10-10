770c2401c42e4b19d816567b0e0e313efcffefda
Dustin Schimel <dustin.schimel@gmail.com>
2017-10-10 12:22:56 -0600

    Changed .gitignore files and replicated the first demo IOS app

---
+++ AloneApp/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  AloneApp
+//
+//  Created by Schimel, Dustin on 10/10/17.
+//  Copyright © 2017 Schimel, Dustin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+    @IBOutlet weak var secondButton: UIButton!
+    
+    @IBAction func secondMethod(_ sender: UIButton)
+    {
+        if ( secondButton.backgroundColor == .purple )
+        {
+            secondButton.backgroundColor = .black
+        }
+        else
+        {
+            secondButton.backgroundColor = .purple
+        }
+        view.backgroundColor = createRandomColor()
+    }
+    
+    private func createRandomColor() -> UIColor
+    {
+        let newColor :UIColor
+        let redValue :CGFloat = CGFloat (Double (arc4random_uniform(256)) / 255.00)
+        let greenValue :CGFloat = CGFloat (Double (arc4random_uniform(256)) / 255.00)
+        let blueValue :CGFloat = CGFloat (Double (arc4random_uniform(256)) / 255.00)
+        newColor = UIColor(red: redValue, green: greenValue, blue: blueValue, alpha: CGFloat(1.0))
+        
+        return newColor
+    }
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
